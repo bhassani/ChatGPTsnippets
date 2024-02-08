b1718dd46d40c01474fbec76a99c00b298ae9793
bhassani <bhassani@users.noreply.github.com>
2024-02-08 12:38:51 +0100

    Create GetImageNtHeaders.cpp

---
+++ Finfisher/GetImageNtHeaders.cpp
@@ -0,0 +1,71 @@
+PIMAGE_NT_HEADERS GetImageNtHeaders(PBYTE pImageBase)
+{
+    PIMAGE_DOS_HEADER pImageDosHeader = (PIMAGE_DOS_HEADER)pImageBase;
+    return (PIMAGE_NT_HEADERS)(pImageBase + pImageDosHeader->e_lfanew);
+}
+PIMAGE_SECTION_HEADER FindRDataSection(PBYTE pImageBase)
+{
+    static const std::string rdata = ".rdata";
+    PIMAGE_NT_HEADERS pImageNtHeaders = GetImageNtHeaders(pImageBase);
+    PIMAGE_SECTION_HEADER pImageSectionHeader = IMAGE_FIRST_SECTION(pImageNtHeaders);
+    int n = 0;
+    for (; n < pImageNtHeaders->FileHeader.NumberOfSections; ++n)
+    {
+        if (rdata == (char*)pImageSectionHeader[n].Name)
+        {
+            break;
+        }
+    }
+    return &pImageSectionHeader[n];
+}
+void CheckGlobalFlagsClearInProcess()
+{
+    PBYTE pImageBase = (PBYTE)GetModuleHandle(NULL);
+    PIMAGE_NT_HEADERS pImageNtHeaders = GetImageNtHeaders(pImageBase);
+    PIMAGE_LOAD_CONFIG_DIRECTORY pImageLoadConfigDirectory = (PIMAGE_LOAD_CONFIG_DIRECTORY)(pImageBase
+        + pImageNtHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_LOAD_CONFIG].VirtualAddress);
+    if (pImageLoadConfigDirectory->GlobalFlagsClear != 0) #  内存中检查IMAGE_DIRECTORY_ENTRY_LOAD_CONFIG
+    {
+        std::cout << "Stop debugging program!" << std::endl;
+        exit(-1);
+    }
+}
+void CheckGlobalFlagsClearInFile()
+{
+    HANDLE hExecutable = INVALID_HANDLE_VALUE;
+    HANDLE hExecutableMapping = NULL;
+    PBYTE pMappedImageBase = NULL;
+    __try
+    {
+        PBYTE pImageBase = (PBYTE)GetModuleHandle(NULL);
+        PIMAGE_SECTION_HEADER pImageSectionHeader = FindRDataSection(pImageBase);
+        TCHAR pszExecutablePath[MAX_PATH];
+        DWORD dwPathLength = GetModuleFileName(NULL, pszExecutablePath, MAX_PATH);
+        if (0 == dwPathLength) __leave;
+        hExecutable = CreateFile(pszExecutablePath, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
+        if (INVALID_HANDLE_VALUE == hExecutable) __leave;
+        hExecutableMapping = CreateFileMapping(hExecutable, NULL, PAGE_READONLY, 0, 0, NULL);
+        if (NULL == hExecutableMapping) __leave;
+        pMappedImageBase = (PBYTE)MapViewOfFile(hExecutableMapping, FILE_MAP_READ, 0, 0,
+            pImageSectionHeader->PointerToRawData + pImageSectionHeader->SizeOfRawData);
+        if (NULL == pMappedImageBase) __leave;
+        PIMAGE_NT_HEADERS pImageNtHeaders = GetImageNtHeaders(pMappedImageBase);
+        PIMAGE_LOAD_CONFIG_DIRECTORY pImageLoadConfigDirectory = (PIMAGE_LOAD_CONFIG_DIRECTORY)(pMappedImageBase 
+            + (pImageSectionHeader->PointerToRawData
+                + (pImageNtHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_LOAD_CONFIG].VirtualAddress - pImageSectionHeader->VirtualAddress)));
+        if (pImageLoadConfigDirectory->GlobalFlagsClear != 0)
+        {
+            std::cout << "Stop debugging program!" << std::endl;
+            exit(-1);
+        }
+    }
+    __finally
+    {
+        if (NULL != pMappedImageBase)
+            UnmapViewOfFile(pMappedImageBase);
+        if (NULL != hExecutableMapping)
+            CloseHandle(hExecutableMapping);
+        if (INVALID_HANDLE_VALUE != hExecutable)
+            CloseHandle(hExecutable);
+    } 
+} 
